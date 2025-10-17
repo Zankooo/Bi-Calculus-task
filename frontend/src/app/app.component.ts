62340747ad90a00c08d81c95aeead7577d726d5b
Zankooo <stankovic.zan@gmail.com>
2025-10-17 23:28:55 +0200

    fixes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HelloService } from './pages/services/hello.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  message = '';
+
+  constructor(private helloService: HelloService) {}
+
+  ngOnInit() {
+    this.helloService.getHello().subscribe({
+      next: (data) => {
+        this.message = data.message;
+      },
+      error: (err) => {
+        console.error('Napaka pri pridobivanju sporočila:', err);
+      },
+    });
+
+
+
+    
+  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,8 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  // { path: '**', redirectTo: 'dashboard' } // (opcijsko)
+  
 ];
